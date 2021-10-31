f5a3e7df47af397f5ca0e0f10d3771989143bbc4
larkvc <448468237@qq.com>
2021-10-31 14:31:10 +0800

    代码提交
    代码提交

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  // BASE_API: '"http://39.96.94.120:38080/app/mock/42//"'
-  // BASE_API: '"http://192.168.3.104:8040/"'
-  BASE_API: '"http://www.fascte.cn:9202/"'
-  //  BASE_API: '"https://www.raise-wisdom.com:8040/"'
+  BASE_API: '"http://39.96.94.120:38080/app/mock/42//"'
 }
